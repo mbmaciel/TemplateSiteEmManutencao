37a218d59ae1452d7acddb4d684f34b57197f2cb
Mauricio Maciel <mbmaciel@gmail.com>
2020-04-02 14:16:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
 		<div class="hr"></div>
 		<h1>Estamos em manutenção.</h1>
 		<p>Nosso site mudou de endereço. Entre agora em rateioestrategico.com.</p>
-
 	</div>
-	</section>
-	</div>
+<footer>
+<!-- Init code pzw.io  //--><script>var $_PowerZAP = { defaultCountry: '+55', widget_id: '20077', company: "313677" }; (function(i,s,o,g,r,a,m){ i[r]={context:{id:'c044980b6706a4e9f4cb04b48ab0fb81'}};a=o;o=s.createElement(o); o.async=1;o.src=g;m=s.getElementsByTagName(a)[0];m.parentNode.insertBefore(o,m); })(window,document,'script','https://js.huggy.chat/widget.min.js?v=8.0.0','pwz');</script><!-- End code pzw.io  //-->
+</footer>	
 </body>
 
 </html>
